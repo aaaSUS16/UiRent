5a6f739ccfb54bb3a4f9b9ca2c8b3f91b4b81907
amirzhenis <amir.zhenis123@mail.ru>
2023-11-29 20:40:01 +0600

    uirent beta1

---
+++ app/src/main/java/com/example/screen_1_activity.java
@@ -0,0 +1,66 @@
+package com.example;
+
+/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		screen_1
+	 *	@date 		Wednesday 29th of November 2023 12:29:12 PM
+	 *	@title 		Page 2
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+import com.example.desingtest1.R;
+
+public class screen_1_activity extends Activity {
+
+	
+	private View _bg__screen_1_ek2;
+	private TextView __________;
+	private TextView ____________;
+	private ImageView untitled_b;
+	private ImageView untitled_b_ek1;
+	private View ellipse_1;
+	private ImageView untitled_b_ek2;
+	private ImageView untitled_b_ek3;
+	private TextView uirent_kz;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.screen_1);
+
+		
+		_bg__screen_1_ek2 = (View) findViewById(R.id._bg__screen_1_ek2);
+		__________ = (TextView) findViewById(R.id.__________);
+		____________ = (TextView) findViewById(R.id.____________);
+		untitled_b = (ImageView) findViewById(R.id.untitled_b);
+		untitled_b_ek1 = (ImageView) findViewById(R.id.untitled_b_ek1);
+		ellipse_1 = (View) findViewById(R.id.ellipse_1);
+		untitled_b_ek2 = (ImageView) findViewById(R.id.untitled_b_ek2);
+		untitled_b_ek3 = (ImageView) findViewById(R.id.untitled_b_ek3);
+		uirent_kz = (TextView) findViewById(R.id.uirent_kz);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
